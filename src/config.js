825d833f0c3f23c23ab2f75671a93afef4a39d2c
John Cipriano <johnmikecip@gmail.com>
2019-03-02 03:49:39 -0800

    move config objet to its own file

---
+++ src/config.js
@@ -0,0 +1,18 @@
+export default {
+  animateMoves: true,
+
+  BG_COLOR: 0x333344,
+  FLOOR_DELAY: (1000 / 10),
+  HEIGHT: 576,
+  LCD_BG_COLOR: 0x301010,
+  MOVE_DELAY: (1000 / 3),
+  MUSIC_VOLUME: 0.25,
+  SPRITE_SHEET: 'assets/tilemaps/sets/felix-big.png',
+  TILE_SIZE: 64,
+  WIDTH: 896,
+
+  DEBUG: false,
+  ENABLE_MUSIC: true,
+  START_LEVEL: 0,
+  START_STATE: 'MainMenu'
+}
--- src/static.js
+++ src/static.js
@@ -1,25 +1,4 @@
 var Chip = Chip || {
-  Mixins: {},
-
-  Config: {
-    animateMoves: true,
-    //
-    BG_COLOR: 0x333344,
-    FLOOR_DELAY: (1000 / 10),
-    HEIGHT: 576,
-    LCD_BG_COLOR: 0x301010,
-    MOVE_DELAY: (1000 / 3),
-    MUSIC_VOLUME: 0.25,
-    SPRITE_SHEET: 'assets/tilemaps/sets/felix-big.png',
-    TILE_SIZE: 64,
-    WIDTH: 896,
-    //
-    DEBUG: false,
-    ENABLE_MUSIC: true,
-    START_LEVEL: 0,
-    START_STATE: 'MainMenu'
-  },
-
   Dir: {
     UP: [0, -1],
     RIGHT: [1, 0],
