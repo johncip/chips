067756efd356214f34de62375a6135ae56620833
John Cipriano <johnmikecip@gmail.com>
2019-03-02 01:38:01 -0800

    add phaser & update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('sup')
+import Phaser from 'phaser'
+
+class Sup extends Phaser.Scene {
+  create() {
+    this.add.text(300, 200, 'sup', { fill: 'red' })
+  }
+}
+
+const config = {
+  width: 680,
+  height: 400,
+  scene: Sup
+}
+
+new Phaser.Game(config)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { DefinePlugin } = require('webpack')
 
 module.exports = {
   mode: 'development',
@@ -32,5 +33,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: 'index.html'
     }),
+    new DefinePlugin({
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true)
+    })
   ]
 }
