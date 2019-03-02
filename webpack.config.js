1465c8bc799258c2263b26ac52c807f41dd5e365
John Cipriano <johnmikecip@gmail.com>
2019-03-02 06:23:41 -0800

    fix webpack-dev-server static stuff dir

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   mode: 'development',
 
   devServer: {
-    contentBase: path.join(__dirname, 'dist')
+    contentBase: path.join(__dirname, 'assets')
   },
 
   devtool: 'source-map',
