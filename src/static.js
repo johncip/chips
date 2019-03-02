bcf7aec715097b937d8157403d127d9d3e27a6a7
John Cipriano <johnmikecip@gmail.com>
2019-03-02 03:40:38 -0800

    remove Function.prototype.includes

--- src/static.js
+++ src/static.js
@@ -172,10 +172,6 @@ Function.prototype.extends = function () {
   }, this)
 }
 
-Function.prototype.includes = function (obj) {
-  _.extend(this.prototype, obj)
-}
-
 String.prototype.toTitleCase = function (str) {
   return this.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
