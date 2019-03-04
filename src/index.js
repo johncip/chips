affd00363cc48c8b74dbf19f286f58ad78de340e
John Cipriano <johnmikecip@gmail.com>
2019-03-04 02:44:36 -0800

    namespace states (create states/index.js)

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import p2 from 'p2'// eslint-disable-line no-unused-vars
 import Phaser from 'phaser'
 
 import config from './config.js'
-import Preload from './states/preload.js' // TODO: namespace states
-import MainMenu from './states/mainmenu.js'
-import Playing from './states/playing.js'
-
+import { Preload, MainMenu, Playing } from './states/index.js'
 
 const game = new Phaser.Game(
   config.width,
---
+++ src/states/index.js
@@ -0,0 +1,9 @@
+import Preload from './preload.js'
+import MainMenu from './mainmenu.js'
+import Playing from './playing.js'
+
+export {
+  Preload,
+  MainMenu,
+  Playing
+}
