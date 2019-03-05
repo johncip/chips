560ad5812a4f786614bc94d0b5a020464f6267c9
John Cipriano <johnmikecip@gmail.com>
2019-03-05 03:31:45 -0800

    refactor LCD (es6 class & extract helpers)

--- src/lcd.js
+++ src/lcd.js
@@ -1,62 +1,63 @@
-import { extend } from 'lodash'
 import sprintf from 'sprintf'
-
 import config from './config'
 
-const { tsize, lcdBgColor } = config
-const headingStyle = {
-  font: '20px lato',
-  fill: '#ddddee'
-}
 
-function LCD (game, labelText) {
-  this.game = game
-  this.font = game.add.retroFont('lcd', 64, 100, '0123456789', 0)
-  this.group = game.add.group()
+/*
+ * A 3-digit LCD display with a heading above it.
+ */
+export default class LCD {
+  constructor (game, labelText) {
+    this.font = game.add.retroFont('lcd', 64, 100, '0123456789', 0)
+    this.display = 0
+    this.group = game.add.group()
 
-  this.createLabel(labelText)
-  this.createBackground()
-  this.createDigits()
-
-  this.display = 0
-}
-
-extend(LCD.prototype, {
-  createLabel: function (text) {
-    const label = this.game.add.text(0, 0, text, headingStyle)
-    this.group.add(label)
-  },
-
-  createBackground: function () {
-    const padding = tsize / 4
-    const g = this.game.add.graphics()
-    const left = padding * -0.5
-    const top = tsize * 0.4
-    const width = tsize * 3 + padding
-    const height = tsize * 1.5 + padding
-    const radius = tsize / 12
-    roundRect(g, left, top, width, height, radius)
-    this.group.add(g)
-  },
-
-  createDigits: function () {
-    const digits = this.game.add.image(0, tsize * 0.5, this.font)
-    this.group.add(digits)
+    this.group.addMultiple([
+      background(game),
+      heading(game, labelText),
+      digits(game, this.font)
+    ])
   }
-})
 
-Object.defineProperty(LCD.prototype, 'display', {
-  get: function () {
+  get display () {
     return this.font.text
-  },
+  }
 
-  set: function (num) {
+  set display (num) {
     this.font.text = sprintf('%03d', num)
   }
-})
+}
 
-function roundRect (g, left, top, width, height, rad) {
-  g.beginFill(lcdBgColor, 1.0)
+function digits (game, font) {
+  return game.add.image(0, config.tsize * 0.5, font)
+}
+
+function heading (game, text) {
+  const headingStyle = {
+    font: '20px lato',
+    fill: '#ddddee'
+  }
+  return game.add.text(0, 0, text, headingStyle)
+}
+
+function background (game) {
+  const g = game.add.graphics()
+  const { tsize } = config
+  const padding = tsize / 4
+  const left = padding * -0.5
+  const top = tsize * 0.4
+  const width = tsize * 3 + padding
+  const height = tsize * 1.5 + padding
+  const radius = tsize / 12
+  roundRect(g, left, top, width, height, radius, config.lcdBgColor)
+  return g
+}
+
+
+/*
+ * Returns a rounded rectangle.
+ */
+function roundRect (g, left, top, width, height, rad, color) {
+  g.beginFill(color, 1.0)
   g.moveTo(left + rad, top)
   g.lineTo(left + width - rad, top)
   g.quadraticCurveTo(left + width, top, left + width, top + rad)
@@ -68,5 +69,3 @@ function roundRect (g, left, top, width, height, rad) {
   g.quadraticCurveTo(left, top, left + rad, top)
   g.endFill()
 }
-
-export default LCD
