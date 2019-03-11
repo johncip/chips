a6ecdae63f2a18e8b182b2a34e5d49ec5db01bbb
John Cipriano <johnmikecip@gmail.com>
2019-03-11 04:17:43 -0700

    refactor Sidebar

--- src/sidebar.js
+++ src/sidebar.js
@@ -3,39 +3,21 @@ import { each } from 'lodash'
 import config from './config'
 import LCD from './lcd'
 
+/**
+ * The sidebar is a container for the 3 LCDs.
+ */
 export default class Sidebar {
   constructor (game) {
     this.game = game
-    this.group = game.add.group()
-    this.group.fixedToCamera = true
-    this.createMask()
-    this.createLcds()
-  }
 
-  createLcds () {
-    const { tsize } = config
+    const group = game.add.group()
+    group.fixedToCamera = true
 
-    this.lcds = {
-      level: new LCD(this.game, 'Level'),
-      time: new LCD(this.game, 'Time'),
-      chips: new LCD(this.game, 'Chips')
-    }
+    const mask = createMask(game)
+    group.add(mask)
 
-    let top = 0.1 * tsize
-    each(this.lcds, (lcd) => {
-      lcd.group.x += 10.25 * tsize
-      lcd.group.y = top
-      top += tsize * 2.25
-      this.group.add(lcd.group)
-    })
-  }
-
-  createMask () {
-    const { tsize, bgColor } = config
-    const mask = this.game.add.graphics()
-    mask.beginFill(bgColor, 1.0)
-    mask.drawRect(9 * tsize, 0, 5 * tsize, 9 * tsize)
-    this.group.add(mask)
+    this.lcds = createLcds(game)
+    each(this.lcds, lcd => this.group.add(lcd.group))
   }
 
   setChipsLeft (val) {
@@ -50,3 +32,29 @@ export default class Sidebar {
     this.lcds.chips.display = val
   }
 }
+
+const { tsize, bgColor } = config
+
+function createMask (game) {
+  const mask = game.add.graphics()
+  mask.beginFill(bgColor, 1.0)
+  mask.drawRect(9 * tsize, 0, 5 * tsize, 9 * tsize)
+
+  return mask
+}
+
+function createLcds (game) {
+  this.lcds = {
+    level: new LCD(this.game, 'Level'),
+    time: new LCD(this.game, 'Time'),
+    chips: new LCD(this.game, 'Chips')
+  }
+
+  let top = 0.1 * config.tsize
+  each(this.lcds, (lcd) => {
+    lcd.group.x += 10.25 * config.tsize
+    lcd.group.y = top
+    top += tsize * 2.25
+  })
+}
+
