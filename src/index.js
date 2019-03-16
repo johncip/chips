a503df87a199e17c72b14e2685a1209dec5a9de6
John Cipriano <johnmikecip@gmail.com>
2019-03-16 03:16:02 -0700

    auto-scale game

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import Phaser from 'phaser'
 import config from './config'
 import { Preload, Playing } from './scenes'
-
 import 'Assets/style/style.css'
+
+
+const ratio = window.devicePixelRatio || 1
 
 // eslint-disable-next-line no-new
 new Phaser.Game({
   type: Phaser.AUTO,
+  parent: 'gameContainer',
   width: config.width,
   height: config.height,
-  parent: 'gameContainer',
-  resolution: window.devicePixelRatio || 1,
-  scene: [Preload, Playing]
+  resolution: ratio,
+  scene: [Preload, Playing],
+  scale: {
+    parent: 'gameContainer',
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: config.width / ratio,
+    height: config.height / ratio
+  }
 })
 
 require('./music')
