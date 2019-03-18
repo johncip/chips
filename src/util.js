2d88869729961d2e51cfa89a877c90d10d712649
John Cipriano <johnmikecip@gmail.com>
2019-03-18 13:15:33 -0700

    use reduce for invert & move to util.js

--- src/util.js
+++ src/util.js
@@ -1,3 +1,21 @@
+import { reduce } from 'lodash'
+
+/*
+ * Returns the inverse of an object. The inverse will have the original's
+ * keys as its values, and the values as its keys. The original should be
+ * one-to-one and have strings for values.
+ *
+ * Converts integer-strings to integers.
+ */
+export function invert (obj) {
+  const reducer = (acc, val, key) => {
+    acc[val] = isNaN(key) ? key : parseInt(key)
+    return acc
+  }
+
+  return reduce(obj, reducer, {})
+}
+
 /*
  * Increments n, modulo base.
  */
