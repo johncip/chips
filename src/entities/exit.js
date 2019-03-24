3d16a812b8034032d490647d5417e1c503374caf
John Cipriano <johnmikecip@gmail.com>
2019-03-23 18:11:33 -0700

    exit.js whitespace

--- src/entities/exit.js
+++ src/entities/exit.js
@@ -7,17 +7,18 @@ import Entity from './entity'
 export default class Exit extends Entity {
   constructor (scene, tile, entityMap) {
     super(scene, tile, entityMap)
+
     this.entityMap.exit = this // TODO: don't do these here
 
-    const frames = scene.anims.generateFrameNumbers('sprites', { frames: [73, 80] })
-
+    const frames = scene.anims.generateFrameNumbers('sprites', {
+      frames: [73, 80]
+    })
     this.pulse = scene.anims.create({
       key: 'pulse',
       frames,
       frameRate: 2,
       repeat: -1
     })
-
     this.sprite.anims.load(this.pulse)
     this.sprite.play('pulse')
   }
